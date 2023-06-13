722f352aac37ce4cf1d65fe2caebdfafe0dd71e6
johwanhui <kikiki0611@gmail.com>
2023-06-14 00:18:55 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+const MAX_POINTS: u64 = 100_000;
+
+
+fn main() {
+    let x = 5;
+    let mut x = x * 5;
+    x = x * 3;
+    let x = x - 10;
+    println!("the value is {}", x);
+
+    func2();
+
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        10
+    };
+
+    println!("The value of y is: {}", y);
+    println!("The value of y is: {}", five());
+}
+
+fn func2() {
+    print!("func2");
+}
+
+fn five() -> i32 {
+    5
+}
